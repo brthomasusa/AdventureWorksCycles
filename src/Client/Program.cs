d7245354bfdeb3914e838f3ef08b781b3825ebbb
brthomasusa <brian.thomas@btechnical-consulting.com>
2023-05-25 09:23:51 -0500

    Added 3rd party library dependencies

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazorise();
+builder.Services.AddFluentValidation();
+builder.Services.AddFluxor();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
