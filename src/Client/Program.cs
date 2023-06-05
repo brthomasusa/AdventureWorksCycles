5ee3a56f3bcf0bd4efa3fe7f2291e732ee2eba16
brthomasusa <brian.thomas@btechnical-consulting.com>
2023-06-05 09:16:59 -0500

    Moving over to Windows

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,6 +1,7 @@
+using AWC.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using AWC.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazorise();
 builder.Services.AddFluentValidation();
 builder.Services.AddFluxor();
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
