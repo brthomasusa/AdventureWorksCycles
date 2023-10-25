f4dc194108b3d51b3876ffaba88b702a70432c1a
brthomasusa <brian.thomas@btechnical-consulting.com>
2023-10-25 17:18:46 -0500

    Set env variable so that passwords can be removed from git

--- test/IntegrationTest/AppSettingsTests.cs
+++ test/IntegrationTest/AppSettingsTests.cs
@@ -18,5 +18,23 @@ namespace AWC.IntegrationTest
             config.GetConnectionString("DefaultConnection")
                 .ShouldEqual("Server=tcp:mssql-server,1433;Database=AdventureWorks_Test;User Id=sa;Password=Info99Gum;TrustServerCertificate=True");
         }
+
+        [Fact]
+        public void Should_Get_ConnectionString_From_Env_Variable()
+        {
+            string? connstr = Environment.GetEnvironmentVariable("ConnectionStrings__TestConnection");
+
+            Assert.NotNull(connstr);
+            Assert.Equal("Server=tcp:mssql-server,1433;Database=AdventureWorks_Test;User Id=sa;Password=Info99Gum;TrustServerCertificate=True", connstr);
+        }
+
+        [Fact]
+        public void Should_Get_DevelopmentEnvironment_From_Env_Variable()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            Assert.NotNull(env);
+            Assert.Equal("Development", env);
+        }
     }
 }
